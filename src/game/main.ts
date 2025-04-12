8e5e99711dd599d0a0cdc9babad625ed6fec19d0
yanickkarst <yanick@DESKTOP-JMPGJA6>
2025-04-12 02:42:01 +0200

    :repeat: allow touch screen events

--- src/game/main.ts
+++ src/game/main.ts
@@ -4,18 +4,22 @@ import { SolitaireScene } from "./scenes/Game";
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
-  type: Phaser.AUTO,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    width: window.innerWidth,
-    height: window.innerHeight,
-  },
-  scene: SolitaireScene,
-  backgroundColor: "#2d2d2d",
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        width: window.innerWidth,
+        height: window.innerHeight,
+    },
+    scene: SolitaireScene,
+    backgroundColor: "#2d2d2d",
+    input: {
+        touch: true,
+        mouse: true
+    }
 };
 
 const StartGame = (parent: string) => {
-  return new Game({ ...config, parent });
+    return new Game({ ...config, parent });
 };
 
 export default StartGame;
